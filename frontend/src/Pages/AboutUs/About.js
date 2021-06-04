a3051a98b6ee0f3fbf355eb509afed818e62cb66
Miguel Liu Li <liu4008@hotmail.com>
2021-06-03 22:48:28 -0400

    Update About.js

--- frontend/src/Pages/AboutUs/About.js
+++ frontend/src/Pages/AboutUs/About.js
@@ -1,1 +1,13 @@
-test
+import React from 'react';
+import './About.css';
+
+export default class Homepage extends React.Component {
+    render() {
+        return (
+            <div>
+                <p> Hello this is Miguel </p>
+                <h1 className="hello">Hello home</h1>
+            </div>
+        )
+    }
+}
