c9919c895b2fa958d898f3ead4b088cac6044a6d
Miguel Liu Li <liu4008@hotmail.com>
2021-06-07 02:32:29 -0400

    updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,20 @@
-import Homepage from "./Pages/Homepage/Homepage";
-import Auth from "./Pages/Auth/Auth";
+import About from "./Pages/AboutUs/About";
+// import Homepage from "./Pages/Homepage/Homepage";
+
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      This is the starter point for Feedi. Everything will be show up in here.
-      <Homepage />
-      <Auth />
-      /** FJ in system */
+      {/* This is the starter point for Feedi. Everything will be show up in here. */}
+      {/* begin miguel part */}
+      <About />
+      {/* END of Miguel Part end point  */}
+      {/* <Homepage /> */}
+
+
+      
     </div>
   );
 }
